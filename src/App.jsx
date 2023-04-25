051cdb5d0a2ca87d66bb21d32c570d4b8cc2eb7b
Allan Oliveira <allanoliveira.dev@gmail.com>
2023-04-25 02:14:40 -0300

    create header module css pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
 
 export function App() {
 
   return (
     <div>
+      <Header />
       <Post
         author="Allan Moreto"
         content="lorLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit eum officia unde, voluptate non corrupti cumque? Sapiente, quasi ipsa quis ut praesentium ipsam molestiae dolor vitae. Molestiae, incidunt consequatur!em"
