65c0363ddb39f066c2269a9f89cdab7d5d04ab73
Allan Oliveira <allanoliveira.dev@gmail.com>
2023-04-26 03:42:32 -0300

    create sidebar component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
 
+import styles from './App.module.css'
+import './global.css'
+import { Sidebar } from './components/Sidebar'
+
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post
-        author="Allan Moreto"
-        content="lorLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit eum officia unde, voluptate non corrupti cumque? Sapiente, quasi ipsa quis ut praesentium ipsam molestiae dolor vitae. Molestiae, incidunt consequatur!em"
-      />
-      <Post
-        author="SIngletary"
-        content="MOrem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit eum officia unde, voluptate non corrupti cumque? Sapiente, quasi ipsa quis ut praesentium ipsam molestiae dolor vitae. Molestiae, incidunt consequatur!em"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+          author="Allan Moreto"
+          content="lorLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit eum officia unde, voluptate non corrupti cumque? Sapiente, quasi ipsa quis ut praesentium ipsam molestiae dolor vitae. Molestiae, incidunt consequatur!em"
+         />
+          <Post
+            author="SIngletary"
+            content="MOrem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit eum officia unde, voluptate non corrupti cumque? Sapiente, quasi ipsa quis ut praesentium ipsam molestiae dolor vitae. Molestiae, incidunt consequatur!em"
+          />
+        </main>
+      </div>
     </div>
 
   )
