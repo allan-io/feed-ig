5712b514d3aca99bf71a6174d8b0916647a91d68
Allan Oliveira <allanoliveira.dev@gmail.com>
2023-04-27 00:18:00 -0300

    create post and comment components

---
+++ src/components/Post.jsx
@@ -0,0 +1,42 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img
+                        className={styles.avatar}
+                        src="https://github.com/allan-io.png"
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>Allan Moreto</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='11 de Maio as 11:13' dateTime="2022-05-11 00:23:12">Publicado ha 1hr</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href=""> jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{'  '}
+                    <a href="#">#nlw</a>{'  '}
+                    <a href="#">#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Leave your Comment</strong>
+                <textarea
+                    placeholder="Leave a Comment"
+                />
+                <button type="submit">Comment</button>
+            </form>
+        </article>
+
+    )
+}
