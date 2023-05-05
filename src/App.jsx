6072b5e46852b951a9934570967b1d9229540938
Allan Oliveira <allanoliveira.dev@gmail.com>
2023-05-05 03:42:46 -0300

    add iterations of posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,42 @@ import styles from './App.module.css'
 import './global.css'
 import { Sidebar } from './components/Sidebar'
 
+// author: {avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/allan-io.png',
+      name: 'Allan Moreto',
+      role: 'freelance dev'
+    },
+    content: [
+      { type: 'paragraph', content: 'what is going on everyone!!' },
+      { type: 'paragraph', content: 'this is not how things are done in real life' },
+      { type: 'link', content: 'allan.moretodev' }
+    ],
+    publishedAt: new Date('2023-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/HeyMichelle.png',
+      name: 'Michelle',
+      role: 'junior dev'
+    },
+    content: [
+      { type: 'paragraph', content: 'I do not find this to be correct' },
+      { type: 'paragraph', content: 'the word excellent does not begin to describe this!' },
+      { type: 'link', content: 'comehere' }
+    ],
+    publishedAt: new Date('2023-05-04 22:00:00')
+  }
+]
+
 export function App() {
-
-  console.log('hi world')
-
   return (
     <div>
       <Header />
@@ -16,14 +48,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-          author="Allan Moreto"
-          content="lorLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit eum officia unde, voluptate non corrupti cumque? Sapiente, quasi ipsa quis ut praesentium ipsam molestiae dolor vitae. Molestiae, incidunt consequatur!em"
-         />
-          <Post
-            author="SIngletary"
-            content="MOrem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit eum officia unde, voluptate non corrupti cumque? Sapiente, quasi ipsa quis ut praesentium ipsam molestiae dolor vitae. Molestiae, incidunt consequatur!em"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
